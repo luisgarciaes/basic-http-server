1fbd0a035629b3bed4ca64aebbbf9ca077b1aa95
ruruisu <zaddozgl@gmail.com>
2022-03-19 19:45:25 -0600

    commenting

--- app.js
+++ app.js
@@ -6,4 +6,8 @@ http.createServer(function(req,res){
     res.end('Hello World\n');
 
     
-}).listen(1337,'127.0.0.1')
+}).listen(1337,'127.0.0.1')// this seems to start a local web instance with some simple text content
+
+// it runs as long as you don't exit the process in the CLI
+
+//the headers seem to contain some metadata about the page/server
