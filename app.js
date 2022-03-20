a4c01282ed9d6664d48d3655f3d9327422dddf88
ruruisu <zaddozgl@gmail.com>
2022-03-20 17:47:41 -0600

    arrow function refactor

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
-http.createServer(function(req,res){
+http.createServer((req,res)=>{ //arrow function
 
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World\n');
